df8fc8f2e43eedc0aaac0d6c644d3f2852edf22e
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2022-03-13 18:29:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+import heroku3
+from telethon import TelegramClient, events
+#
+# Burayı gurcalama
+# 
+# 
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+
+# Telethon 
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+#
+bot_username = os.environ.get("BOT_USERNAAME")
+owner_username = os.environ.get("OWNER_USERNAAME")
